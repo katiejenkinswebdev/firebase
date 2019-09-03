92eaecf2b13ab8aaa78674ec7cc3036dba8d6f69
katiejenkinswebdev <katiejenkinswebdev@gmail.com>
2019-09-03 14:06:57 -0600

    signup with email and password posting to db

--- public/js/auth.js
+++ public/js/auth.js
@@ -14,17 +14,45 @@ console.log('loaded');
 
 // Initialize Firebase once DOM is loaded
 document.addEventListener("DOMContentLoaded", event => {
-  const app = firebase.app();
+  try {
+      const app = firebase.app();
+  } catch (e) {
+      console.error(e);
+  }
 });
 
 const googleLogin = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
 
-  //async, will give us user when promise resolves with .then()
+  // Async, will give us user when promise resolves with .then()
+  // Will create JWT for user. In browser, type localStorage to view.
+  // TODO In IndexedDB, there is fbLocalStorage in fbase_key. Confirm JWT is here and UID is unique, doesn't change
   firebase.auth().signInWithPopup(provider)
     .then(result => {
       const user = result.user;
 
       document.write(`Hello ${user.displayName}`);
     })
+    .catch(error => {
+      console.log(error)
+    })
+};
+
+const emailPasswordLogin = () => {
+
+  const email = document.getElementById('email').value,
+    password = document.getElementById('password').value;
+    console.log(email, password);
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(result => {
+      console.log('result', result);
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      console.log('error code', errorCode);
+      var errorMessage = error.message;
+      console.log('error message', errorMessage);
+  });
 };
