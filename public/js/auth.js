87908515343146f26c4ca2c4ec79273030a50272
katiejenkinswebdev <katiejenkinswebdev@gmail.com>
2019-09-03 17:53:35 -0600

    add reset password and email verification tools

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,59 +2,100 @@
 document.addEventListener("DOMContentLoaded", event => {
   try {
       const app = firebase.app();
+
+      // FirebaseUI config.
+      var uiConfig = {
+        signInSuccessUrl: 'success.html',
+        signInOptions: [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        ],
+        tosUrl: 'tos.html',
+        privacyPolicyUrl: 'privacyPolicy.html',
+      };
+
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
+
   } catch (e) {
       console.error(e);
   }
 });
 
-const googleLogin = () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
-
-  // Async, will give us user when promise resolves with .then()
-  // Will create JWT for user. In browser, type localStorage to view.
-  // TODO In IndexedDB, there is fbLocalStorage in fbase_key. Confirm JWT is here and UID is unique, doesn't change
-  firebase.auth().signInWithPopup(provider)
-    .then(result => {
-      const user = result.user;
-
-      document.write(`Hello ${user.displayName}`);
-    })
-    .catch(error => {
-      console.log(error)
-    })
-};
-
 const emailPasswordSignup = () => {
-
   const emailSignup = document.getElementById('emailSignup').value,
     passwordSignup = document.getElementById('passwordSignup').value;
 
   firebase.auth().createUserWithEmailAndPassword(emailSignup, passwordSignup)
     .then(result => {
-      console.log('result', result);
+      checkAuthStatus();
+      var user = firebase.auth().currentUser;
+
+      user.sendEmailVerification().then(function() {
+        // Email sent.
+        console.log('email sent');
+      }).catch(function(error) {
+        // An error happened.
+      });
     })
     .catch(function(error) {
       // Handle Errors here.
       let errorCode = error.code;
-      console.log('error code', errorCode);
       let errorMessage = error.message;
-      console.log('error message', errorMessage);
   });
 };
 
 const emailPasswordLogin = () => {
-
   const emailLogin = document.getElementById('emailLogin').value,
     passwordLogin = document.getElementById('passwordLogin').value;
-    console.log(emailLogin, passwordLogin);
 
   firebase.auth().signInWithEmailAndPassword(emailLogin, passwordLogin)
     .then(result => {
       const user = result.user;
-      document.write(`Hello ${user}`);
+      checkAuthStatus();
     })
     .catch(function(error) {
       let errorCode = error.code;
       let errorMessage = error.message;
     });
+};
+
+const checkAuthStatus = () => {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      console.log('user signed in');
+      let displayName = user.displayName;
+      let email = user.email;
+      let emailVerified = user.emailVerified;
+      let photoURL = user.photoURL;
+      let isAnonymous = user.isAnonymous;
+      let uid = user.uid;
+      let providerData = user.providerData;
+    } else {
+      // User is signed out.
+      console.log('user is signed out');
+    }
+  });
+};
+
+const sendResetPasswordEmail = () => {
+  let emailAddress = firebase.auth().currentUser.email;
+
+  firebase.auth().sendPasswordResetEmail(emailAddress).then(function() {
+    // Email sent.
+  }).catch(function(error) {
+    // An error happened.
+  });
+};
+
+const signOut = () => {
+  firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+  checkAuthStatus();
+
+  }).catch(function(error) {
+    // An error happened.
+  });
 }
